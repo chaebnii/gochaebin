bf47116694ca00da4d14ff42b94cc0f3c2c9fc83
chaebin <leebin199206@gmail.com>
2024-04-19 16:50:37 +0900

    Intelli J first

--- src/main/java/com/test/test/controller/testController.java
+++ src/main/java/com/test/test/controller/testController.java
@@ -4,9 +4,11 @@ import com.test.test.entity.Board;
 import com.test.test.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class testController {
@@ -24,14 +26,22 @@ public class testController {
     }
 
     @PostMapping("/test/write1") //글쓰기 클릭 서버 부분
-   public String boardWrite1(Board board){
+   public String boardWrite1(Board board ,Model model){
 
         boardService.write(board);
+        model.addAttribute("board",boardService.boardList());
+
         System.out.println("제목:"+board.getTitle());
         System.out.println("내용은:"+board.getContent());
-        return "boardWrite1";
+        return "BoardList";
     }
 
+    @GetMapping("/test/view")
+    public String boardView(@RequestParam(name="id") Integer id, Model model){
+        System.out.println("aha");
+        model.addAttribute("board",boardService.BoardView(id));
+        return "BoardView";
+    }
 
 
 }
--- src/main/java/com/test/test/service/BoardService.java
+++ src/main/java/com/test/test/service/BoardService.java
@@ -5,13 +5,27 @@ import com.test.test.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
     public void write(Board board){
         boardRepository.save(board);
-
     }
+
+    //게시글 모두 불러오기
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    // 게시글 내용 불러오기
+    public Board BoardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
 
 }
